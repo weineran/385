03cae56305e739b3e9c7d271577b074e931cb7fc
Zachary Rice <zachrice9@gmail.com>
2015-02-23 10:12:36 -0600

    nicer formatting

--- exp5/control.sv
+++ exp5/control.sv
@@ -15,7 +15,7 @@ always_ff @ (posedge Clk or posedge Reset )
 			curr_state = A; 	// A is the reset/start state
 		else
 			curr_state = next_state;
-end
+	
 
 always_comb
 begin
@@ -31,195 +31,189 @@ begin
 		F : next_state = G;
 		H : next_state = I;
 		I : next_state = J;
-        K : next_state = L;
-        L : next_state = M;
-        M : next_state = N;
-        N : next_state = O;
-        O : next_state = P;
-        P : if(~Execute)
-            next_state = A;       		
+      K : next_state = L;
+      L : next_state = M;
+      M : next_state = N;
+      N : next_state = O;
+      O : next_state = P;
+      P : if(~Execute)
+          next_state = A;       		
 				
-endcase
-end
+	case
+	
 
 always_comb
 begin
 	case(curr_state)
-		A: begin
-			Add = 1'b0;
-			Sub = 1'b0;
-			Shift = 1'b0;
-            if(ClearA_LoadB)
-            begin
-				Clr_Ld = 1'b1;
-			end
-            else
-			begin	
-                Clr_ld = 1'b0;
-			end
-		   end
+		  A:  begin
+				Add = 1'b0;
+				Sub = 1'b0;
+				Shift = 1'b0;
+				if(ClearA_LoadB)
+					Clr_Ld = 1'b1;  
+				else
+					Clr_ld = 1'b0;
+				end
 
 
         //1 
-		B: begin
-			if(M)
-				Add = 1'b1;
-            else
-				Add = 1'b0; 
-				Sub = 1'b0;
-				Clr_ld = 1'b0;
-				Shift = 1'b0;
-		    end
-		// first shift
-		C: begin
-			Shift = 1'b1;
-			Add = 1'b0;
-			Sub = 1'b0;
-			Clr_ld = 1'b0;
-			end
-		
-        //2
-        D: begin
-			if(M)
-				Add = 1'b1;
-			else
+		  B: 	begin
+				if(M)
+					Add = 1'b1;
+				else
+					Add = 1'b0; 
+					Sub = 1'b0;
+					Clr_ld = 1'b0;
+					Shift = 1'b0;
+					
+		  // first shift
+		  C:  begin
+				Shift = 1'b1;
 				Add = 1'b0;
 				Sub = 1'b0;
 				Clr_ld = 1'b0;
-				Shift = 1'b0;
-			end
-		// second shift
-		E: begin
-			Shift = 1'b1;
-			Add = 1'b0;
-			Sub = 1'b0;
-			Clr_ld = 1'b0;
-            end
+					
+		
+        //2
+        D:  begin
+				if(M)
+					Add = 1'b1;
+				else
+					Add = 1'b0;
+					Sub = 1'b0;
+					Clr_ld = 1'b0;
+					Shift = 1'b0;
+					
+		  // second shift
+		  E:  begin
+			   Shift = 1'b1;
+			   Add = 1'b0;
+			   Sub = 1'b0;
+			   Clr_ld = 1'b0;
+					
 
-	    //3
-        F: begin
-            if(M)
-                Add = 1'b1;
-            end
-            else
-                Add = 1'b0;
-                Sub = 1'b0;
-                Clr_ld = 1'b0;
-                Shift = 1'b0;    
-            end
+	     //3
+        F:  begin
+			   if(M)
+					Add = 1'b1;
+			   else
+					Add = 1'b0;
+               Sub = 1'b0;
+               Clr_ld = 1'b0;
+               Shift = 1'b0;    
+					
         // third shift
-        G: begin
+        G:  begin
             Shift = 1'b1;
             Add = 1'b0;
             Sub = 1'b0;
             Clr_ld = 1'b0;
-            end
+					
 
         //4
-        H: begin
+        H:  begin
             if(M)
                 Add = 1'b1;
-            end
+					
             else
                 Add = 1'b0;
                 Sub = 1'b0;
                 Clr_ld = 1'b0;
                 Shift = 1'b0;    
-            end
+					
         // fourth shift
-        I: begin
+        I:  begin
             Shift = 1'b1;
             Add = 1'b0;
             Sub = 1'b0;
             Clr_ld = 1'b0;
-            end
+					
         
         //5
-        H: begin
+        H:  begin
             if(M)
                 Add = 1'b1;
-            end
+					
             else
                 Add = 1'b0;
                 Sub = 1'b0;
                 Clr_ld = 1'b0;
                 Shift = 1'b0;    
-            end
+					
         // fifth shift
-        I: begin
+        I:  begin
             Shift = 1'b1;
             Add = 1'b0;
             Sub = 1'b0;
             Clr_ld = 1'b0;
-            end
+					
 
          //6
-        J: begin
+        J:  begin
             if(M)
                 Add = 1'b1;
-            end
+					
             else
                 Add = 1'b0;
                 Sub = 1'b0;
-                Clr_ld = 1'b0;
+                Clr_ld = 1'b0;end
                 Shift = 1'b0;    
-            end
+					
         // sixth shift
-        K: begin
+        K:  begin
             Shift = 1'b1;
             Add = 1'b0;
             Sub = 1'b0;
             Clr_ld = 1'b0;
-            end
+					
 
         //7
-        L: begin
+        L:  begin
             if(M)
                 Add = 1'b1;
-            end
             else
                 Add = 1'b0;
                 Sub = 1'b0;
                 Clr_ld = 1'b0;
                 Shift = 1'b0;    
-            end
+					
         // seventh shift
-        M: begin
+        M:  begin
             Shift = 1'b1;
             Add = 1'b0;
             Sub = 1'b0;
             Clr_ld = 1'b0;
-            end
+					
         
         //Subtract if necessary
-        N: begin
-            if(M) begin
+        N:  begin
+            if(M)
                 Sub = 1'b1;
             else
                 Sub = 0'b0;
                 Add = 1'b0;
                 Shift = 1'b1;
                 Clr_ld = 1'b0;
-            end
+					
         // Last shift
-        O: begin
+        O:  begin
             Add = 1'b0;
             Sub = 1'b0;
             Shift = 1'b1;
             Clr_ld = 1'b0;
-            end
+					
 
         // clear
-        P: begin
+        P:  begin
             Add = 1'b0;
             Sub = 1'b0;
             Shift = 1'b0;
             Clr_ld = 1'b0;
-            end
-        endcase
+					
+			case
 
-end
-endmodule
+	
+	module
 
 
 
