1c3d8a327a1a2e50f0080bb9788b816a0003809e
Zachary Rice <zachrice9@gmail.com>
2015-03-03 18:24:33 -0600

    data_path change

--- exp6/data_path.sv
+++ exp6/data_path.sv
@@ -4,6 +4,7 @@ module data_path
 (
 	//input logic [15:0] S,
 	input logic Clk, Reset, Run, Continue, load_ir, load_pc, load_mdr,load_mar,
+	input logic [1:0] pc_sel, 
 	//output logic [11:0] LED,
 	output logic [19:0] ADDR,
 	inout logic [15:0] Data
@@ -12,6 +13,9 @@ module data_path
 logic [15:0] mdr_out;
 logic [15:0] pc_out;
 logic [15:0] pc_in;
+logic [15:0] pc_mux_out;
+logic [15:0] decode_mux_out;
+
 
 ir inst_reg
 (
@@ -32,7 +36,7 @@ register pc
 (
 	.clk(Clk),
 	.load(load_pc),
-	.in(pc_in),
+	.in(pc_mux_out),
 	.out(pc_out)
 );
 
@@ -63,6 +67,17 @@ tri_buff pc_buff
 	.sel(load_pc),
 	.out(Data)
 );
+
+mux4 pc_mux
+(
+	.a(Data),
+	.b(pc_in),
+	.c(decode_mux_out), 		// still figuring out preceeding logic for this input
+	.d(16b'0000000000000000),
+	.sel(pc_sel),
+	.f(pc_mux_out)
+);
+
 
 
 endmodule
