cd41231a16c8d1ff360c71161295bff12022601c
Andrew <weiner.andrew.j@gmail.com>
2015-03-04 22:11:21 -0600

    andrew put tri state buff back to ugly

--- exp6/tri_buff.sv
+++ exp6/tri_buff.sv
@@ -3,17 +3,10 @@ module tri_buff
 	input [15:0] in,
 	input sel,
 	output [15:0] out
-	
 );
 
 
-always_comb
-begin
-	if (sel==0)
-		out = 16'bZZZZZZZZZZZZZZZZ;
-	else
-		out = in;
-end
+assign out = (sel) ? in : 4'hzzzz;
 
 
 endmodule
